e1a58284a28eb7c040903f02ca120ae2dc7fd3ad
mikeyfreake <mikeyfreake@gmail.com>
2015-04-18 08:37:34 -0600

    jQuery and AngularJS versions
    Added a second index page. Now one is jQuery based and the other is
    AngularJS based.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(function() {
+	var app = angular.module('fantasyFeed',[]);
+	
+	app.controller('rssFeed', function() {
+		
+	});
+})();
--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-(function() {
-	var app = angular.module('fantasyFeed',[]);
+$(document).ready(function(){
+	console.log('DOM loaded. Loading RSS feeds...');
+	parseRSS('http://www.rotoworld.com/rss/feed.aspx?sport=nfl', showFeed);
+});
+
+function parseRSS(url, callback) {
+  $.ajax({
+    //url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+	url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+    dataType: 'json',
+    success: function(data) {
+      callback(data.responseData.feed);
+    }
+  });
+}
+
+function showFeed(data) {
 	
-	app.controller('rssFeed', function() {
-		
-	});
-})();
+}
